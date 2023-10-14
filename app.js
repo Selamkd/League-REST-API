4d365f74de7a439deb911fd4c032a828eca1bc93
Selam Ararsa <131351836+Selamkd@users.noreply.github.com>
2023-10-14 15:02:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,108 @@
+//Import express, nodemon, fs and UUID
+import express from "express";
+import nodemon from "nodemon";
+import morgan from "morgan";
+import {v4 as uuidv4} from "uuid";
+
+
+//set port number
+const PORT = 4000;
+const app = express();
+
+// bring in CRUD functions from champions.js
+import {
+    getChampions,
+    getChampionByID,
+    getChampionByName,
+    addChampion,
+    // editChampionData,
+    deleteChampion,
+} from "./champions.js";
+
+//Set up modules for use
+app.use(express.json());
+app.use(morgan('dev'));
+//app.use(cors('dev')) *add back in if we add frontend*
+
+
+//add port listening
+app.listen(PORT, function(){
+    console.log(`Sever is now listening on http://localhost:${PORT}`);
+})
+
+//add welcome message to test
+app.get("/", function (req, res){
+    res.send("Welcome to Team 31's League of Legends API!");
+})
+ 
+// get all champions
+app.get("/champions", async function(req, res) {
+    const champions  = await getChampions()
+    const response = {
+         status: "success",
+         data:champions,
+    }
+   return  res.status(200).json(champions)
+    });
+
+    // get a specific champion by it's id 
+app.get("/champions/id/:id", async function(req, res) {
+        const id = req.params.id;
+      
+        
+          const champion = await getChampionByID(id);
+          
+          if (!champion) {
+            return res.status(404).json({ error: 'Champion could not be found' });
+          }
+      
+          const response = {
+            status: "success",
+            data: champion,
+          };
+      
+          return res.status(200).json(response);
+        });
+// get a specific champion by it's name 
+app.get("/champions/name/:championName", async function(req, res) {
+    const name = req.params.championName;
+          
+   
+    const champion = await getChampionByName(name);
+             console.log(champion);
+        const response = {
+        status: "success",
+        data: champion,
+        };
+          
+    return res.status(200).json(response);
+    });
+
+// add new champion object to the data 
+app.post("/champions", async function(req, res) {
+ 
+  // get user data from req.body;
+  const newChampion = req.body;
+  // call the createUser() function to a new user variable
+  let createdChampion =  await addChampion(newChampion)
+
+  // return with a created user data and a 201 created status 
+
+  const response = {
+    status:'success',
+    data:createdChampion,
+  }
+  return res.status(201).json(response)
+});
+
+// delete a champion's object by it's id
+app.delete("/champions/:id", async function(req,res){
+
+    const id = req.params.id;
+    const deletedChampion = await getChampionByID(id);
+    if(!deletedChampion){
+      return res.status(404).json({ error: 'Champion not found' });
+    }else{
+      await deleteChampion(id)
+      res.status(200).json({ message: 'Champion deleted successfully', champion: deletedChampion});
+    }});
---
+++ champions.js
@@ -0,0 +1,121 @@
+// Import the file manager system
+import {promises as fs} from "node:fs";
+import { get } from "node:http";
+import path from "node:path";
+import {v4 as uuidv4} from "uuid";
+
+// set the file path 
+const filepath = path.resolve(process.cwd(), "champions.json")
+
+// function to add a new champion
+export async function addChampion(newChampion) {
+  const champions = await fs.readFile("champions.json", "utf-8");
+  const championsJSON = JSON.parse(champions);
+  const newChampionData = {
+    id:uuidv4(),
+    championName: newChampion.championName,
+    abilities: newChampion.abilities,
+    ulitimateAbility:newChampion.ulitimateAbility,
+    region: newChampion.region,
+    difficulty: newChampion.difficulty,
+    role: newChampion.role,
+    range: newChampion.range,
+    lore: newChampion.lore
+  };
+  championsJSON.push(newChampionData);
+  const updatedChampions = JSON.stringify(championsJSON, null, 2);
+  await fs.writeFile(filepath, updatedChampions, "utf-8");
+
+  return newChampionData;
+
+}
+
+//Function to get the list of all the champions
+  export async function getChampions() {
+    const championsJSON = await fs.readFile("champions.json", "utf-8");
+    const champions = JSON.parse(championsJSON);
+    return champions;
+  }
+
+// Function to get all the champions with a particular ID
+  export async function getChampionByID(id) {
+    const championsJSON = await fs.readFile("champions.json", "utf-8");
+    const champions = JSON.parse(championsJSON);
+  
+//If ID matches, return the champion object
+    for (const champion of champions) {
+      if (champion.id === id) {
+        return champion;
+      }
+    }
+// If no match, return null
+    return null;
+  }
+
+  // Function to get all the champions with a particular name
+
+  export async function getChampionByName(name) {
+    try {
+        const championsJSON = await fs.readFile(filepath, "utf-8");
+        const champions = JSON.parse(championsJSON);
+
+        // Search for a champion with a matching name (case-insensitive)
+        const foundChampion = champions.find(champion =>
+            champion.championName === name
+        )
+   
+        return foundChampion;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+await getChampionByName('Briar');
+
+
+export async function deleteChampion (ID) {
+    const championsJSON = await fs.readFile("champions.json", "utf-8");
+    const champions = JSON.parse(championsJSON);
+    let championIndex = null;
+    for(let i = 0; i < champions.length; i++){
+        if (champions[i].id === ID){
+        championIndex = i;
+        break;
+        }
+    }
+    if (championIndex !== null) {
+    const champions = JSON.parse(championsJSON)
+    const deleteChampion = champions.splice(championIndex, 1);
+    await fs.writeFile (filepath, JSON.stringify (champions, null, 2), "utf-8");
+    return deleteChampion[0];
+}}
+
+
+
+// export async function editChampionData(id, newName, newRegion) {
+//     const championsJSON = await fs.readFile("champions.js", "utf-8");
+//     const champions = JSON.parse(championsJSON);
+//     // Find the champion by ID
+//     const champion = champions.find(champ => champ.id === id);
+//     if (!champion) {
+//       console.log('Champion not found');
+//     }
+//     // Update champion data if new values are provided
+//     if (newName !== undefined) {
+//       champion.championName = newName;
+//     }
+//     if (newRegion !== undefined) {
+//       champion.region = newRegion;
+//     }
+//     // Write the updated data back to the file
+//     await fs.writeFile("champions.js", JSON.stringify(champions, null, 2), "utf-8");
+//     return champion;
+//   }
+
+
+
+
+
+
+
